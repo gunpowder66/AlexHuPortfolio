ac46824519e484524c558bdce038b121fe9fb14a
Alex Hu <alexhumeow@gmail.com>
2022-05-09 00:34:05 -0700

    project page progress

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,8 @@ import styles from '../styles/Home.module.css'
 import { Card, Paper } from '@mui/material';
 
 export default function LandingPage() {
-  const [imgHover, setImgHover] = useState(false);
-
-  const onHover = () => {
-    setImgHover(true);
-  }
-
-  const onExit = () => {
-    setImgHover(false);
-  }
-
+  const [val, setVal] = useState(false);
+  const [course, setCourse] = useState(false);
 
 
   return (
@@ -28,13 +20,82 @@ export default function LandingPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Card className={styles.card} raised={false}>
-        <div className={styles.gif}>
-          <Image src={'/gifs/Valorankings_voting.gif'} alt='gif' layout='fill' objectFit="contain"/>
-        </div>
-      </Card>
+      <div className={styles.cardDiv} >
+        <Card
+        className={styles.card}
+        id='classmates-card'
+        onMouseEnter={() => setCourse(true)}
+        onMouseLeave={() => setCourse(false)}>
+          <div className={styles.gif}>
+            {
+              (course)
+              ? <Image src={'/gifs/classmates.gif'} alt='gif' layout='fill' objectFit="contain"/>
+              : <Image src={'/classmatesfreeze.png'} alt='gif' layout='fill' objectFit='contain' />
+            }
+          </div>
+          <div className={styles.title}>Classmates</div>
+          <div className={styles.description}>
+          Mentorship-based remote learning platform that connects mentors and mentees around the world
+          <br/>
+          <br/>
+          Built with:
+          <br/>
+          Node, Nextjs, Firebase, MUI, and Jest
+          </div>
+        </Card>
+      </div>
 
+      <div className={styles.cardDiv} >
+        <Card
+        className={styles.card}
+        id='val-card'
+        onMouseEnter={() => setVal(true)}
+        onMouseLeave={() => setVal(false)}>
+          <div className={styles.gif}>
+            {
+              (val)
+              ? <Image src={'/gifs/Valorankings_voting.gif'} alt='gif' layout='fill' objectFit="contain"/>
+              : <Image src={'/staticimg_val.png'} alt='gif' layout='fill' objectFit='contain' />
+            }
+          </div>
+          <div className={styles.title}>ValoRankings</div>
+          <div className={styles.description}>
+          Full stack crowdsourcing web application that utilizes a voting system to gather real time data and show popularity rankings for all cosmetic items used in the game Valorant.
+          <br/>
+          <br/>
+          Built with:
+          <br/>
+          Node, React, Express, and Postgres
+          </div>
+        </Card>
+      </div>
 
+      <div className={styles.cardDiv} >
+        <Card
+        className={styles.card}
+        id='kloth-card'
+        onMouseEnter={() => setVal(true)}
+        onMouseLeave={() => setVal(false)}>
+          <div className={styles.klothGif}>
+            {
+              (val)
+              ? <Image src={'/gifs/reviews_filters.gif'} alt='gif' layout='fill' objectFit="contain"/>
+              : <Image src={'/klothfreeze.png'} alt='gif' layout='fill' objectFit='contain' />
+            }
+          </div>
+          <div className={styles.Klothtitle}>Kloth</div>
+          <div className={styles.Klothdescription}>
+          A single-page, front-end web application of the product page for an e-commerce website
+          <br/>
+          <br/>
+          Built with:
+          <br/>
+          Node, React, Express, and MUI
+          </div>
+        </Card>
+      </div>
+
+      <div style={{marginTop: '5%'}}>MEOW</div>
     </div>
   )
 }
